b7c9eddb1a85285a7018a48621f0a31df6904b19
mrkaye97 <mrkaye97@gmail.com>
2021-12-29 13:50:57 -0500

    test the status of the API
    This will wake up the Heroku app if it's sleeping, too

---
+++ tests/testthat/test-api-is-up.R
@@ -0,0 +1,30 @@
+test_that("API is up", {
+
+  .alive <- function() {
+    tryCatch(
+      check_api_status(),
+      error = function(e) "not ok"
+    )
+  }
+
+  .difftime <- function(starttime, currtime = Sys.time()) {
+    as.numeric(currtime - starttime)
+  }
+
+  status <- ""
+  starttime <- Sys.time()
+  tries <- 0
+
+  while(status != "ok" & (.difftime(starttime) < 180)) {
+
+    if (tries > 0) Sys.sleep(10)
+    status <- .alive()
+
+    tries <- tries + 1
+  }
+
+  expect_equal(
+    status,
+    "ok"
+  )
+})
