3b8404154b1e08312492cb3b18ba80cdad4e87dd
mrkaye97 <mrkaye97@gmail.com>
2022-01-17 16:17:34 -0500

    cleanup

--- R/project.R
+++ R/project.R
@@ -20,8 +20,8 @@
 #' @return Returns a list of tags for the `run_id`
 #' @export
 set_git_tracking_tags <- function(
-  run_id,
-  client,
+  run_id = get_active_run_id(),
+  client = mlflow_client(),
   git_repo_url = Sys.getenv("GIT_REPO_URL"),
   git_repo_subdir,
   source_git_commit = system("git rev-parse HEAD", intern = TRUE),
@@ -29,11 +29,6 @@ set_git_tracking_tags <- function(
   project_entrypoint = "main",
   project_backend = "local"
 ) {
-
-  .args <- resolve_args(
-    run_id = maybe_missing(run_id),
-    client = maybe_missing(client)
-  )
 
   source_name <- paste(
     git_repo_url,
@@ -44,54 +39,54 @@ set_git_tracking_tags <- function(
   set_tag(
     key = "mlflow.source.git.repoURL",
     value = git_repo_url,
-    run_id = .args$run_id,
-    client = .args$client
+    run_id = run_id,
+    client = client
   )
 
   set_tag(
     key = "mlflow.source.type",
     value = "PROJECT",
-    run_id = .args$run_id,
-    client = .args$client
+    run_id = run_id,
+    client = client
   )
 
   set_tag(
     key = "mlflow.source.git.commit",
     value = source_git_commit,
-    run_id = .args$run_id,
-    client = .args$client
+    run_id = run_id,
+    client = client
   )
 
   set_tag(
     key = "mlflow.source.git.branch",
     value = source_git_branch,
-    run_id = .args$run_id,
-    client = .args$client
+    run_id = run_id,
+    client = client
   )
 
   set_tag(
     key = "mlflow.source.name",
     value = source_name,
-    run_id = .args$run_id,
-    client = .args$client
+    run_id = run_id,
+    client = client
   )
 
   set_tag(
     "mlflow.project.entryPoint",
     project_entrypoint,
-    run_id = .args$run_id,
-    client = .args$client
+    run_id = run_id,
+    client = client
   )
 
   set_tag(
     "mlflow.project.backend",
     project_backend,
-    run_id = .args$run_id,
-    client = .args$client
+    run_id = run_id,
+    client = client
   )
 
   get_run(
-    run_id = .args$run_id,
-    client = .args$client
+    run_id = run_id,
+    client = client
   )$tags[[1]]
 }
