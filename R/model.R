a0f01f4ac384e48e65c291b893921be7ffa72fd7
mrkaye97 <mrkaye97@gmail.com>
2022-02-24 16:05:03 -0500

    helpers

--- R/model.R
+++ R/model.R
@@ -88,13 +88,20 @@ write_model_spec <- function(path, content) {
 
 #' @importFrom withr with_options
 get_timestamp <- function() {
-  with_options(
-    c(digits.secs = 2),
-    format(
-      as.POSIXlt(Sys.time(), tz = "UTC"),
-      "%y-%m-%dT%H:%M:%S.%OS"
-    )
+  format(
+    as.POSIXlt(Sys.time(), tz = "UTC"),
+    "%y-%m-%dT%H:%M:%S.%OS"
   )
+}
+
+convert_timestamp_to_ms <- function(timestamp) {
+  ts <- as.POSIXct(
+    timestamp,
+    format = "%y-%m-%dT%H:%M:%S.%OS",
+    tz = "UTC"
+  )
+
+  as.integer(ts) * 1000
 }
 
 #' Load MLflow Model
